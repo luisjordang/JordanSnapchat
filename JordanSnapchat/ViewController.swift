48758065fb2d9b2ba880c59b6942071c2d19eed5
luisjordang <luis.jordan@tecsup.edu.pe>
2024-05-29 12:47:18 -0500

    Proyecto ya enlazado con Auth a Firebase

--- JordanSnapchat/ViewController.swift
+++ JordanSnapchat/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {(user, error) in
+            print("Intentando iniciar sesion")
+            if error != nil{
+                print("Se presento el siguiente error: \(error)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+        
+    }
+    
 }
 
